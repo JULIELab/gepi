bd12dba55d7e71ef1db3feaf225df7e22abab018
matthies <franz.matthies@uni-jena.de>
2017-01-18 13:29:30 +0100

    some minor work

--- jules-bioc-pmc-db-import/runimport.sh
+++ jules-bioc-pmc-db-import/runimport.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z $JAVA_BIN ]; then
+	JAVA_BIN="java"
+fi
+
 export CLASSPATH=`echo lib/* | tr ' ' ':'`:conf:target/classes:resources
 export UIMA_JVM_OPTS="-Xmx10g -Dfile.encoding=UTF-8"
-runCPE.sh desc/CPE.xml
+
+$JAVA_BIN $UIMA_JVM_OPTS -cp $CLASSPATH de.julielab.jules.cpe.CPERunner desc/CPE.xml
