c6d662412e76a6134a26af0f247d48a01c706a2c
SchSascha <s.schaeuble@gmail.com>
2017-12-13 14:11:46 +0100

    CHANGE: Code improvements based on pull request review.

--- gepi/gepi-core/src/main/java/de/julielab/gepi/core/retrieval/services/EventPostProcessingService.java
+++ gepi/gepi-core/src/main/java/de/julielab/gepi/core/retrieval/services/EventPostProcessingService.java
@@ -2,11 +2,10 @@ package de.julielab.gepi.core.retrieval.services;
 
 import static org.neo4j.driver.v1.Values.parameters;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.tapestry5.annotations.Log;
@@ -20,6 +19,7 @@ import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.driver.v1.Transaction;
 import org.neo4j.driver.v1.TransactionWork;
 
+import de.julielab.gepi.core.retrieval.data.Argument;
 import de.julielab.gepi.core.retrieval.data.Event;
 
 public class EventPostProcessingService implements IEventPostProcessingService {
@@ -36,42 +36,23 @@ public class EventPostProcessingService implements IEventPostProcessingService {
 	@Log
 	@Override
 	public List<Event> setPreferredNameFromGeneId(List<Event> ev) {
-		// the following hashmaps gather information of which gene is appearing 
-		// in which event(s)
-		// two maps are created to differentiate between first and second argument
-		HashMap<String, List<Event>> geneIdEvtMap1stArg, geneIdEvtMap2ndArg;
 		// the following hashmap maps gene ids as they appear in the previous hashmaps
 		// to their respective preferred name as it is written in the neo4j database
-		HashMap<String, String> geneIdPrefNameMap;
-
-		geneIdEvtMap1stArg = new HashMap<String, List<Event>>();
-		geneIdEvtMap2ndArg = new HashMap<String, List<Event>>();
-		setGeneIdEvtMap(ev, geneIdEvtMap1stArg, geneIdEvtMap2ndArg);
-		// get preferred names from neo4j database
-		Set<String> geneIds = new HashSet<String>( geneIdEvtMap1stArg.size() + geneIdEvtMap2ndArg.size() );
-		geneIds.addAll(geneIdEvtMap1stArg.keySet());
-		geneIds.addAll(geneIdEvtMap2ndArg.keySet());
-		geneIdPrefNameMap = new HashMap<String, String>();		
-		getGeneIdPrefNameMap(geneIdPrefNameMap, geneIds);
+		Map<String, String> geneIdPrefNameMap = new HashMap<>();
 		
-		// associate the correct preferred name to the event
-		String tmpPrefName = new String();
-		// check 1st arguments
-		for (String key : geneIdEvtMap1stArg.keySet()) {
-			tmpPrefName = geneIdPrefNameMap.get(key);
-			Iterator<Event> it = geneIdEvtMap1stArg.get(key).iterator();		
-			while (it.hasNext()) {
-				it.next().getFirstArgument().setPreferredName(tmpPrefName);
+		Set<Argument> args = new HashSet<Argument>();
+		Set<String> geneIds = new HashSet<String>();
+		for (Event e : ev )
+			for (Argument a : e.getArguments()) {
+				args.add(a);
+				geneIds.add( a.getGeneId() );
 			}
-		}
-		// check 2nd arguments
-		for (String key : geneIdEvtMap2ndArg.keySet()) {
-			tmpPrefName = geneIdPrefNameMap.get(key);
-			Iterator<Event> it = geneIdEvtMap2ndArg.get(key).iterator();		
-			while (it.hasNext()) {
-				it.next().getSecondArgument().setPreferredName(tmpPrefName);
-			}
-		}
+		
+		// get preferred names from neo4j database
+		geneIdPrefNameMap = getGeneIdPrefNameMap(geneIdPrefNameMap, geneIds);
+		
+		for (Argument a : args)
+			a.setPreferredName(geneIdPrefNameMap.get( a.getGeneId() ));
 		
 		return ev;
 	}
@@ -83,7 +64,7 @@ public class EventPostProcessingService implements IEventPostProcessingService {
 	 * @param geneIdPrefNameMap
 	 * @param geneIds
 	 */
-	private void getGeneIdPrefNameMap(HashMap<String, String> geneIdPrefNameMap, Set<String> geneIds) {
+	private Map<String, String> getGeneIdPrefNameMap(Map<String, String> geneIdPrefNameMap, Set<String> geneIds) {
 		
 		Config neo4jconf = Config.build().withoutEncryption().toConfig();
 		Driver driver = GraphDatabase.driver(this.BASE_NEO4J_URL, 
@@ -91,9 +72,9 @@ public class EventPostProcessingService implements IEventPostProcessingService {
 		
 		try (Session session = driver.session()) {
 
-			session.readTransaction(new TransactionWork<HashMap<String, String>>() {
+			session.readTransaction(new TransactionWork<Map<String, String>>() {
 				@Override
-				public HashMap<String, String> execute(Transaction tx) {
+				public Map<String, String> execute(Transaction tx) {
 					Record record;
 					String[] searchInput = new String[geneIds.size()];
 					searchInput = geneIds.toArray(new String[geneIds.size()]);
@@ -117,43 +98,8 @@ public class EventPostProcessingService implements IEventPostProcessingService {
 			});
 		}
 		
-	}
-	
-	
-	/**
-	 * Create a map from entrez gene ids to to events.
-	 * The reason to use gene ids is that of now not all genes in our database are associated to a 
-	 * top homology atid.
-	 * @param ev - List of events
-	 */
-	private void setGeneIdEvtMap(List<Event> ev, 
-			HashMap<String, List<Event>> geneIdEvtMap1stArg, 
-			HashMap<String, List<Event>> geneIdEvtMap2ndArg) {
-		Event currentEvt;
-		String tmpTHAtid;
-				
-		Iterator<Event> it = ev.iterator();
-		while (it.hasNext()) {
-			currentEvt = it.next();
-			// first argument
-			tmpTHAtid  = currentEvt.getFirstArgument().getGeneId();
-			if ( geneIdEvtMap1stArg.containsKey( tmpTHAtid ) )
-				geneIdEvtMap1stArg.get(tmpTHAtid).add(currentEvt);
-			else {
-				List<Event> tmpList = new ArrayList<Event>();
-				tmpList.add(currentEvt);
-				geneIdEvtMap1stArg.put(tmpTHAtid, tmpList);				
-			}
-			// 2nd argument
-			tmpTHAtid  = currentEvt.getSecondArgument().getGeneId();
-			if ( geneIdEvtMap2ndArg.containsKey( tmpTHAtid ) )
-				geneIdEvtMap2ndArg.get(tmpTHAtid).add(currentEvt);
-			else {
-				List<Event> tmpList = new ArrayList<Event>();
-				tmpList.add(currentEvt);
-				geneIdEvtMap2ndArg.put(tmpTHAtid, tmpList);
-			}
-		}		
+		return geneIdPrefNameMap;
+		
 	}
 
 }
