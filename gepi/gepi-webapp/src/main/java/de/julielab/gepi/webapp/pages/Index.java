d3fb2f9b3b98b110ff4f8a016a9bf5e36f4cceb1
khituras <chew@gmx.net>
2018-11-08 17:59:25 +0100

    Updates Bootstrap to 3.3.7. Added a mockup for intactivity options.

--- gepi/gepi-webapp/src/main/java/de/julielab/gepi/webapp/pages/Index.java
+++ gepi/gepi-webapp/src/main/java/de/julielab/gepi/webapp/pages/Index.java
@@ -24,7 +24,7 @@ import de.julielab.gepi.core.retrieval.data.EventRetrievalResult;
 /**
  * Start page of application gepi-webapp.
  */
-@Import(stylesheet = {"context:css-pages/index.less"})
+@Import(stylesheet = {"context:css-pages/index.less"}, library = {"context:mybootstrap/js/dropdown.js"})
 public class Index {
     @Inject
     private Logger logger;
