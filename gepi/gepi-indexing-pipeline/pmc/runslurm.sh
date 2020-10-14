985944ad2834b3d30f05199365781cfc4f04ed4f
khituras <chew@gmx.net>
2020-10-14 08:49:30 +0200

    Preparing to create a common indexing project for shared classes.

--- gepi/gepi-indexing-pipeline/pmc/runslurm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --mem 25G
-#SBATCH --cpus-per-task 7 
-#SBATCH -J GepiIndexPMC 
-$JAVA_HOME/bin/java -jar ~/bin/jcore-pipeline-runner-base* run.xml 
-
--- gepi/gepi-indexing-pipeline/pubmed/runslurm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --mem 50G
-#SBATCH --cpus-per-task 7 
-#SBATCH -J GEPIINDEX 
-$JAVA_HOME/bin/java -jar ~/bin/jcore-pipeline-runner-base* run.xml
-
