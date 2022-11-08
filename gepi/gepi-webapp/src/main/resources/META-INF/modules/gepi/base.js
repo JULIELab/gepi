47d87caa36f0882707163953be11a7b25d923483
khituras <chew@gmx.net>
2022-11-08 17:55:59 +0100

    Re-enable tooltips after ajax requests.

--- gepi/gepi-webapp/src/main/resources/META-INF/modules/gepi/base.js
+++ gepi/gepi-webapp/src/main/resources/META-INF/modules/gepi/base.js
@@ -1,9 +1,17 @@
-define(["bootstrap5/tooltip"], function(Tooltip) {
+define(["jquery", "bootstrap5/tooltip"], function($, Tooltip) {
 	var setuptooltips = function() {
+		const makeTooltips = function() {
 		var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="default-tooltip"]'))
         var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
           return new Tooltip(tooltipTriggerEl, {html:true})
         })
+     };
+     // re-enable tooltips after Ajax requests that might bring in new
+     // elements into the DOM that should have tooltips
+     $(document).ajaxComplete(function() {
+     		makeTooltips();
+     })
+     makeTooltips();
 	};
 	return {
 		"setuptooltips" : setuptooltips
--- gepi/gepi-webapp/src/main/resources/META-INF/modules/gepi/charts/tablewidget.js
+++ gepi/gepi-webapp/src/main/resources/META-INF/modules/gepi/charts/tablewidget.js
@@ -1,38 +1,58 @@
 define(["jquery", "bootstrap5/modal", "bootstrap5/tooltip"], function($, modal, Tooltip) {
 	$('#downloadModal').modal();
-	const download = function(downloadUrl){
-		$('#downloadButton').on('click', function(){
+	const download = function(downloadUrl) {
+		$('#downloadButton').on('click', function() {
 			$('#downloadModal').modal('toggle');
 			console.log("Downloading data from " + downloadUrl)
 
 			// from https://stackoverflow.com/a/23013574/1314955
 			var link = document.createElement("a");
-   			// If you don't know the name or want to use
-   			// the webserver default set name = ''
-   			name=''
-   			link.setAttribute('download', name);
-   			link.href = downloadUrl;
-   			document.body.appendChild(link);
-   			link.click();
-   			console.log("Download window popped up")
-   			link.remove();
-		})};
+			// If you don't know the name or want to use
+			// the webserver default set name = ''
+			name = ''
+			link.setAttribute('download', name);
+			link.href = downloadUrl;
+			document.body.appendChild(link);
+			link.click();
+			console.log("Download window popped up")
+			link.remove();
+		})
+	};
 
 	const setupHighlightTooltips = function() {
-		$(".hl-argument").each(function() {
-			new Tooltip(this, {title: "interaction partner"});
-		});
-		$(".hl-trigger").each(function() {
-			new Tooltip(this, {title: "interaction indicator word"});
-		})
-		$(".hl-filter").each(function() {
-			new Tooltip(this, {title: "fulltext filter match"});
-		})
+		const makeTooltips = function() {
+			$(".hl-argument").each(function() {
+				new Tooltip(this, {
+					title: "interaction partner"
+				});
+			});
+			$(".hl-trigger").each(function() {
+				new Tooltip(this, {
+					title: "interaction indicator word"
+				});
+			})
+			$(".hl-filter").each(function() {
+				new Tooltip(this, {
+					title: "fulltext filter match"
+				});
+			})
+		}
+
+		// Register the tooltip-function with ajaxComplete because
+		// after using a paging button, the table gets updated
+		// using Ajax and we need to re-enable the tooltips
+		// on the new table page.
+		$(document).ajaxComplete(function() {
+				makeTooltips();
+			}
+
+		);
+		makeTooltips();
 	}
+
 
 	return {
 		download: download,
 		setupHighlightTooltips: setupHighlightTooltips
 	}
 })
-
