19db7921992929b1e1a3bd1c247e3af98123a7c6
khituras <chew@gmx.net>
2022-12-20 12:22:22 +0100

    Set production mode back to true. Perhaps the issue was not that value but the fact that I didn't update the server side code from git.

--- gepi/gepi-webapp/src/main/java/de/julielab/gepi/webapp/services/AppModule.java
+++ gepi/gepi-webapp/src/main/java/de/julielab/gepi/webapp/services/AppModule.java
@@ -64,7 +64,7 @@ public class AppModule {
 
         // This is something that should be removed when going to production, but is useful
         // in the early stages of development.
-        configuration.override(SymbolConstants.PRODUCTION_MODE, false);
+        configuration.override(SymbolConstants.PRODUCTION_MODE, true);
     }
 
     public static void contributeApplicationDefaults(
