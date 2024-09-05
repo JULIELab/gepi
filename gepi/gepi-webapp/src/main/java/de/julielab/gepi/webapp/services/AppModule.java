2a384fbf4e1ff8e045fe800456ab718ef26b8d3f
khituras <chew@gmx.net>
2024-09-05 20:53:34 +0200

    Use overrides for CORS symbols

--- gepi/gepi-webapp/src/main/java/de/julielab/gepi/webapp/services/AppModule.java
+++ gepi/gepi-webapp/src/main/java/de/julielab/gepi/webapp/services/AppModule.java
@@ -82,11 +82,11 @@ public class AppModule {
         // configuration file.
         configuration.add("gepi.maintenance", false);
 
-        configuration.add(SymbolConstants.CORS_ENABLED, "true");
-        configuration.add(SymbolConstants.CORS_ALLOWED_ORIGINS, "*");
-        configuration.add(SymbolConstants.CORS_ALLOW_CREDENTIALS, "true");
-        configuration.add(SymbolConstants.CORS_EXPOSE_HEADERS, "X-Custom-Header");
-        configuration.add(SymbolConstants.CORS_MAX_AGE, "3600");
+        configuration.override(SymbolConstants.CORS_ENABLED, "true");
+        configuration.override(SymbolConstants.CORS_ALLOWED_ORIGINS, "*");
+        configuration.override(SymbolConstants.CORS_ALLOW_CREDENTIALS, "true");
+        configuration.override(SymbolConstants.CORS_EXPOSE_HEADERS, "X-Custom-Header");
+        configuration.override(SymbolConstants.CORS_MAX_AGE, "3600");
     }
 
     public static void contributeApplicationDefaults(
